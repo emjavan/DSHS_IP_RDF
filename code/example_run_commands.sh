937b6e671e04a818aa10b0a1b1507139b4a0b40a
Emily Javan, MS PhD <emjavan@gmail.com>
2024-09-23 17:19:31 -0500

    update with ls6 test command

--- code/example_run_commands.sh
+++ code/example_run_commands.sh
@@ -1,5 +1,13 @@
 # Example run commmands
 
-bash filter_icd10_codes.sh \
+# local machine testing with synthetic data
+time bash filter_icd10_codes.sh \
 ../synthetic_data/IP_RDF_synthetic_data.txt \
+../input_data/icd10_disease_category_list.csv
+
+# Lonestar6 testing with a real subset of the patient data
+# This line is copied into the launch_filter_icd10_codes.sh 
+#  NOT run on the login node!!!
+time bash filter_icd10_codes.sh \
+../../data_subsets_for_testing/head50lines_out.IP_2018_Q3_4.txt \
 ../input_data/icd10_disease_category_list.csv
--- code/launch_filter_icd10_codes.sh
+++ code/launch_filter_icd10_codes.sh
@@ -6,7 +6,7 @@
 #SBATCH -p corralextra                  # Queue (partition) name
 #SBATCH -N 1                            # Total nodes (must be 1 for serial)
 #SBATCH -n 1                            # Total mpi tasks (should be 1 for serial)
-#SBATCH -t 01:00:00                     # Run time (hh:mm:ss)
+#SBATCH -t 00:30:00                     # Run time (hh:mm:ss)
 #SBATCH --mail-type=all                 # Send email at begin, end, fail of job
 #SBATCH -A IBN24016                     # Project/Allocation name (req'd if you have more than 1)
 #SBATCH --mail-user=emjavan@utexas.edu  # Email to send to
@@ -19,4 +19,5 @@ date
 
 # Launch serial code
 time bash filter_icd10_codes.sh \
-../../data_subsets_for_testing/head50lines_out.IP_2018_Q3_4.txt
+../../data_subsets_for_testing/head50lines_out.IP_2018_Q3_4.txt \
+../input_data/icd10_disease_category_list.csv
