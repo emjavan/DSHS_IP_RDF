9fed9c1d463c722c28ae4c64cf0c7013c1a6e672
Emily Javan <emjavan@gmail.com>
2024-09-23 17:06:42 -0500

    update file location for example

--- code/example_run_commands.sh
+++ code/example_run_commands.sh
@@ -1,3 +1,5 @@
 # Example run commmands
 
-bash filter_icd10_codes.sh ../IP_RDF_synthetic_data.txt ../input_data/icd10_disease_category_list.csv
+bash filter_icd10_codes.sh \
+../synthetic_data/IP_RDF_synthetic_data.txt \
+../input_data/icd10_disease_category_list.csv
