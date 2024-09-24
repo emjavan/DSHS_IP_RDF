d00c7959fa886f517b0fc4c347a3dd316d011d90
Emily Javan, MS PhD <emjavan@gmail.com>
2024-09-23 19:57:17 -0500

    add processing of other files

--- code/launch_filter_icd10_codes.sh
+++ code/launch_filter_icd10_codes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH -J test_icd10_filter            # Job name
-#SBATCH -o test_icd10_filter.%j.o       # Name of stdout output file
-#SBATCH -e test_icd10_filter.%j.e       # Name of stderr error file
+#SBATCH -J icd10_filter                 # Job name
+#SBATCH -o icd10_filter.%j.o            # Name of stdout output file
+#SBATCH -e icd10_filter.%j.e            # Name of stderr error file
 #SBATCH -p corralextra                  # Queue (partition) name
 #SBATCH -N 1                            # Total nodes (must be 1 for serial)
 #SBATCH -n 1                            # Total mpi tasks (should be 1 for serial)
@@ -17,8 +17,34 @@
 pwd
 date
 
+# Leaving as serial task because 500GB of data won't fit in RAM
 # Launch serial code
+
+# Total rows before filtering: 1555126
+# Total rows after filtering: 8991
+# Took 5.835s
+#time bash filter_icd10_codes.sh \
+#../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2018_Q3_4.txt \
+#../input_data/icd10_disease_category_list.csv \
+#../../FILTERED_PAT_FILES/out.IP_2018_Q3_4_filtered.txt
+
+
 time bash filter_icd10_codes.sh \
-../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2018_Q3_4.txt \
+../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2019.txt \
 ../input_data/icd10_disease_category_list.csv \
-../../FILTERED_PAT_FILES/out.IP_2018_Q3_4_filtered.txt
+../../FILTERED_PAT_FILES/out.IP_2019_filtered.txt
+
+time bash filter_icd10_codes.sh \
+../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2020.txt \
+../input_data/icd10_disease_category_list.csv \
+../../FILTERED_PAT_FILES/out.IP_2020_filtered.txt
+
+time bash filter_icd10_codes.sh \
+../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2021.txt \
+../input_data/icd10_disease_category_list.csv \
+../../FILTERED_PAT_FILES/out.IP_2021_filtered.txt
+
+time bash filter_icd10_codes.sh \
+../../ALL_OG_FILES/OG_PATIENT_FILES/out.IP_2022.txt \
+../input_data/icd10_disease_category_list.csv \
+../../FILTERED_PAT_FILES/out.IP_2022_filtered.txt
