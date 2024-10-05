f94427272d69ff6ce86c73bb80b66e83548a33ae
Emily Javan, MS PhD <emjavan@gmail.com>
2024-10-05 02:23:38 -0500

    get packages

---
+++ code/get_packages_used.R
@@ -0,0 +1,11 @@
+#/////////////////////////////////
+# Load all the required R packages
+#/////////////////////////////////
+
+# Download the preliminary library
+if (!require("pacman")) install.packages("pacman") # for pacman::p_load
+
+# 
+pacman::p_load(tidyverse,
+               icecream)
+
