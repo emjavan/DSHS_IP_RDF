cd2bd5c56f9ac749d65ff2d633107467ed48efaa
Emily Javan, MS PhD <emjavan@gmail.com>
2024-11-08 09:02:55 -0600

    change file to look for

--- code/launch_disease_count_per_col.sh
+++ code/launch_disease_count_per_col.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH -J sum_disease_counts           # Job name
-#SBATCH -o sum_disease_counts.%j.o      # Name of stdout output file
-#SBATCH -e sum_disease_counts.%j.e      # Name of stderr error file
+#SBATCH -J disease_count                # Job name
+#SBATCH -o disease_count.%j.o           # Name of stdout output file
+#SBATCH -e disease_count.%j.e           # Name of stderr error file
 #SBATCH -p corralextra                  # Queue (partition) name
 #SBATCH -N 1                            # Total nodes (must be 1 for serial)
 #SBATCH -n 1                            # Total mpi tasks (should be 1 for serial)
@@ -18,4 +18,4 @@ mkdir -p "../produced_data/"
 module load Rstats
 
 # Run code to make files and plots
-Rscript disease_count_per_col.R cluster 2018 2022
+Rscript disease_count_per_col.R cluster 2018Q34 2022
